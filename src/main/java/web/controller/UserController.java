c18babfa45af18938766d0970072f6c79b754652
Max Ivanov <rellepro@gmail.com>
2021-02-12 00:21:03 +0300

    Refactor dao service add login and password 1

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/web/security/SecurityConfig.java
+++ src/main/java/web/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
          http.csrf().disable();// - попробуйте выяснить сами, что это даёт
         http.authorizeRequests()
-                .antMatchers("/").permitAll() // доступность всем
+                //.antMatchers("/").permitAll() // доступность всем
                 .antMatchers("/").access("hasAnyRole('ADMIN')") // разрешаем входить на /user пользователям с ролью User
                 .and().formLogin()  // Spring сам подставит свою логин форму
                 .successHandler(successUserHandler); // подключаем наш SuccessHandler для перенеправления по ролям
